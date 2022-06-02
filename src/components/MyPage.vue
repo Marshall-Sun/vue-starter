10133779a835b2da0db5903710aeda154086e4de
Marshall Sun <themarshallsun@gmail.com>
2022-06-02 22:50:03 +0800

    init

---
+++ src/components/MyPage.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="main">
+    <div>785648924576</div>
+    <div>785648924576</div>
+    <div>785648924576</div>
+    <div>
+      785648924576
+    </div>
+    sissisisisisis
+  </div>
+</template>
+
+<style scoped>
+#main {
+  height: 100vh;
+  background: white;
+  overflow: auto;
+}
+div {
+  color: black;
+}
+</style>
